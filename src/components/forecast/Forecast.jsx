af16ae269bd0431621363f828c7057498b6b38be
Andre-se <ibrahimtanriover64@gmail.com>
2023-03-22 17:07:56 +0100

    v4

--- src/components/forecast/Forecast.jsx
+++ src/components/forecast/Forecast.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import "./Forecast.css";
 import {
   Accordion,
   AccordionItem,
@@ -37,10 +37,44 @@ const Forecast = ({ data }) => {
                     className="icon-small"
                   />
                   <label className="day">{forecastDays[idx]}</label>
+                  <label className="desc">{item.weather[0].description}</label>
+                  <label className="tempDay">
+                    {Math.round((item.main.temp_min - 12) / 1.8)}°C /{" "}
+                    {Math.round((item.main.temp_max - 12) / 1.8)}°C
+                  </label>
                 </div>
               </AccordionItemButton>
             </AccordionItemHeading>
-            <AccordionItemPanel></AccordionItemPanel>
+            <AccordionItemPanel>
+              <div className="daily-details-grid">
+                <div className="daily-details-grid-item">
+                  <label>Pressure</label>
+                  <label>{item.main.pressure} hPa</label>
+                </div>
+                <div className="daily-details-grid-item">
+                  <label>Humidity</label>
+                  <label>{item.main.humidity}%</label>
+                </div>
+                <div className="daily-details-grid-item">
+                  <label>Clouds</label>
+                  <label>{item.clouds.all}%</label>
+                </div>
+                <div className="daily-details-grid-item">
+                  <label>Wind Speed:</label>
+                  <label>{item.wind.speed} m/s</label>
+                </div>
+                <div className="daily-details-grid-item">
+                  <label>Sea level:</label>
+                  <label>{item.main.sea_level}m</label>
+                </div>
+                <div className="daily-details-grid-item">
+                  <label>Feels like:</label>
+                  <label>
+                    {Math.round((item.main.feels_like - 12) / 1.6)}°C
+                  </label>
+                </div>
+              </div>
+            </AccordionItemPanel>
           </AccordionItem>
         ))}
       </Accordion>
--- src/components/weater/CurrentWeather.jsx
+++ src/components/weater/CurrentWeather.jsx
@@ -25,20 +25,20 @@ const CurrentWeather = ({ data }) => {
           <div className="parameter-row">
             <span className="parameter-label">Feels like</span>
             <span className="parameter-value">
-              {Math.round(data.main.feels_like)}
+              {Math.round(data.main.feels_like)}°C
             </span>
           </div>
           <div className="parameter-row">
             <span className="parameter-label">Wind</span>
-            <span className="parameter-value">{data.wind.speed}</span>
+            <span className="parameter-value">{data.wind.speed} m/s</span>
           </div>
           <div className="parameter-row">
             <span className="parameter-label">Humidity</span>
-            <span className="parameter-value">{data.main.humidity}</span>
+            <span className="parameter-value">{data.main.humidity}%</span>
           </div>
           <div className="parameter-row">
             <span className="parameter-label">Pressure</span>
-            <span className="parameter-value">{data.main.pressure}</span>
+            <span className="parameter-value">{data.main.pressure} hPa</span>
           </div>
         </div>
       </div>
